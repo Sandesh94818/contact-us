b73e5defc396de7ccf28824a083d26bce5ca107d
Sandesh94818 <41295132+Sandesh94818@users.noreply.github.com>
2021-10-03 21:47:07 +0530

    Added more security
    Added query for checking availability of email address and also fixed some bugs

--- form.php
+++ form.php
@@ -5,19 +5,31 @@ $password="";
 // replace the database name with your database
 $db="contactform"; 
 
-
-
-$conn=mysqli_connect($server,$username,$password,$db) or die("error establishing database connection due to ".mysqli_connect_error());
-if($conn)
 $name=$_POST['name'];
 $email=$_POST['email'];
 $message=$_POST['message'];
 
+$conn=mysqli_connect($server,$username,$password,$db) or die("error establishing database connection due to ".mysqli_connect_error());
 
-$sql="INSERT INTO `form` (`name`, `email`, `message`) VALUES ('$name', '$email', '$message')";
-$result=mysqli_query($conn,$sql);
-if($result)
-echo "submission successful";
+if($_SERVER['REQUEST_METHOD']=='POST' and isset($name)){
+  $query="SELECT * FROM `form` WHERE email='$email'";
+  $res=mysqli_query($conn,$query) or die('Query Failed');
+  if(mysqli_num_rows($res)==0){
+      $sql="INSERT INTO `form` (`name`, `email`, `message`) VALUES ('$name', '$email', '$message')";
+      $result=mysqli_query($conn,$sql) or die("Query Failed");
+    
+      if($result)
+        echo "submission successful.";
+  }else{
+    echo "User with same email ID already Exists."
+  }
+  
+}else{
+  echo "Access Denied."
+}
+
+
+
 
 ?>
 
